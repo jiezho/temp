6751269fa6014e11c417216d0764fe463065c3bc
jiezho <jie.zho@outlook.com>
2019-12-04 00:34:07 +0800

    将python代码进行了一定的拆分，不过后续还要再好好整理一下结构，还是太乱

--- src_py/__init__.py
+++ src_py/__init__.py
@@ -1,66 +1,39 @@
-
-#encoding:utf-8
 #!/usr/bin/env python
-from werkzeug.utils import secure_filename
-from flask import Flask, render_template, jsonify, request, make_response, send_from_directory, abort
-import time
+# -*- coding: utf-8 -*-
+# @Date    : 2019-11-20
+# @Author  : zhoujie
+
 import os
-from strUtil import Pic_str
-import base64
- 
-app = Flask(__name__)
-UPLOAD_FOLDER = 'upload'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+import re
+import json
+
+from datetime import datetime
+from xlrd import xldate_as_tuple
+from flask import Flask, g, jsonify, make_response, request
+from flask_cors import CORS
+from flask_httpauth import HTTPBasicAuth
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from itsdangerous import BadSignature, SignatureExpired
+from passlib.apps import custom_app_context
+import flask_excel as excel
+from werkzeug import SharedDataMiddleware
+from werkzeug.utils import secure_filename
+from flask_sqlalchemy import SQLAlchemy
+
 basedir = os.path.abspath(os.path.dirname(__file__))
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'JPG', 'PNG', 'gif', 'GIF'])
 
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
-
-@app.route('/upload')
-def upload_test():
-    return render_template('up.html')
-
-# 上传文件
-@app.route('/up_photo', methods=['POST'], strict_slashes=False)
-def api_upload():
-    file_dir = os.path.join(basedir, app.config['UPLOAD_FOLDER'])
-    if not os.path.exists(file_dir):
-        os.makedirs(file_dir)
-    f = request.files['photo']
-    if f and allowed_file(f.filename):
-        fname = secure_filename(f.filename)
-        print (fname)
-        ext = fname.rsplit('.', 1)[1]
-        new_filename = Pic_str().create_uuid() + '.' + ext
-        f.save(os.path.join(file_dir, new_filename))
-        return jsonify({"success": 0, "msg": "上传成功"})
-    else:
-        return jsonify({"error": 1001, "msg": "上传失败"})
-
-@app.route('/download/<string:filename>', methods=['GET'])
-def download(filename):
-    if request.method == "GET":
-        if os.path.isfile(os.path.join('upload', filename)):
-            return send_from_directory('upload', filename, as_attachment=True)
-        pass
-    
-# show photo
-@app.route('/show/<string:filename>', methods=['GET'])
-def show_photo(filename):
-    file_dir = os.path.join(basedir, app.config['UPLOAD_FOLDER'])
-    if request.method == 'GET':
-        if filename is None:
-            pass
-        else:
-            image_data = open(os.path.join(file_dir, '%s' % filename), "rb").read()
-            response = make_response(image_data)
-            response.headers['Content-Type'] = 'image/png'
-            return response
-    else:
-        pass
-
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=7000)
-
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_RECORD_QUERIES'] = True
+# 配置不变: 后期可以放在配置文件中
+HOSTNAME = 'localhost'
+PORT = '3306'
+DATABASE = 'rdkx'
+USERNAME = 'root'
+PASSWORD = '000000'
+DB_URI = "mysql+pymysql://{}:{}@{}:{}/{}".format(USERNAME, PASSWORD, HOSTNAME, PORT, DATABASE)
+ 
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
+db = SQLAlchemy(app)
--- src_py/manage.py
+++ src_py/manage.py
@@ -6,14 +6,12 @@
 import os
 import re
 import json
-import xlrd
 
 from datetime import datetime
 from xlrd import xldate_as_tuple
 from flask import Flask, g, jsonify, make_response, request
 from flask_cors import CORS
 from flask_httpauth import HTTPBasicAuth
-from flask_sqlalchemy import SQLAlchemy
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from itsdangerous import BadSignature, SignatureExpired
 from passlib.apps import custom_app_context
@@ -21,183 +19,28 @@ import flask_excel as excel
 from werkzeug import SharedDataMiddleware
 from werkzeug.utils import secure_filename
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+# basedir = os.path.abspath(os.path.dirname(__file__))
 
-app = Flask(__name__)
+# app = Flask(__name__)
+from __init__ import app, db
+import SQLManage
+Admin        = SQLManage.Admin
+JoinInfos    = SQLManage.JoinInfos
+HistoryData  = SQLManage.HistoryData
+listAirClogA = SQLManage.listAirClogA
+listAirClogB = SQLManage.listAirClogB
+listSizeNH3Actual = SQLManage.listSizeNH3Actual
+listSizeNH3Demand = SQLManage.listSizeNH3Demand
+listAirDeposiA = SQLManage.listAirDeposiA
+listAirDeposiB = SQLManage.listAirDeposiB
 
 # r'/*' 是通配符，让本服务器所有的 URL 都允许跨域请求
-# cors = CORS(app, resources={r"/*": {"origins": "*"})
-# CORS(app, resources=r'/*')
+CORS(app, resources=r'/*')
 CORS(app, supports_credentials=True)
-
-app.config['SECRET_KEY'] = 'hard to guess string'
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_RECORD_QUERIES'] = True
-# 配置不变: 后期可以放在配置文件中
-HOSTNAME = 'localhost'
-PORT = '3306'
-DATABASE = 'rdkx'
-USERNAME = 'root'
-PASSWORD = '000000'
-DB_URI = "mysql+pymysql://{}:{}@{}:{}/{}".format(USERNAME, PASSWORD, HOSTNAME, PORT, DATABASE)
- 
-app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
-db = SQLAlchemy(app)
 
 auth = HTTPBasicAuth()
 CSRF_ENABLED = True
 app.debug = True
-
-# 创建导入的数据数据库模型
-class JoinInfos(db.Model):
-    __tablename__ = 'joininfos'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(200), index=True)
-    groupA = db.Column(db.Float)
-    groupB = db.Column(db.Float)
-    unit = db.Column(db.String(64))
-
-    def __repr__(self):
-        return "<JoinInfos:%s>"%self.name
-
-    def to_dict(self):
-        columns = self.__table__.columns.keys()
-        result = {}
-        for key in columns:
-            if key == 'pub_date':
-                value = getattr(self, key).strftime("%Y-%m-%d %H:%M:%S")
-            else:
-                value = getattr(self, key)
-            result[key] = value
-        return result
-
-# ‘登录用户’数据库模型
-class Admin(db.Model):
-    __tablename__ = 'admins'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(32), index=True)
-    password = db.Column(db.String(128))
-
-    # 密码加密
-    def hash_password(self, password):
-        self.password = custom_app_context.encrypt(password)
-
-    # 密码解析
-    def verify_password(self, password):
-        return custom_app_context.verify(password, self.password)
-
-    # 获取token，有效时间100min
-    def generate_auth_token(self, expiration=6000):
-        s = Serializer(app.config['SECRET_KEY'], expires_in=expiration)
-        return s.dumps({'id': self.id})
-
-    # 解析token，确认登录的用户身份
-    @staticmethod
-    def verify_auth_token(token):
-        s = Serializer(app.config['SECRET_KEY'])
-        try:
-            data = s.loads(token)
-        except SignatureExpired:
-            return None  # valid token, but expired
-        except BadSignature:
-            return None  # invalid token
-        admin = Admin.query.get(data['id'])
-        return admin
- 
-# 创建excel导入的runtime数据数据库模型
-class HistoryData(db.Model):
-    __tablename__ = 'historyDatas'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    time = db.Column(db.DateTime, default=datetime.now())
-    airClogA = db.Column(db.Float)
-    airClogB = db.Column(db.Float)
-    sizeNH3Actual = db.Column(db.Float)
-    sizeNH3Demand = db.Column(db.Float)
-    airDeposiA = db.Column(db.Float)
-    airDeposiB = db.Column(db.Float)
-
-    def to_dict(self):
-        columns = self.__table__.columns.keys()
-        result = {}
-        for key in columns:
-            if key == 'time':
-                value = getattr(self, key).strftime("%Y-%m-%d %H:%M:%S")
-            else:
-                value = getattr(self, key)
-            result[key] = value
-        return result
-    
-db.drop_all()
-db.create_all()
-
-# 添加数据
-rdkx_user = Admin(id="8",name="rdkx",password="$5$rounds=535000$9WoOVzcj7Fvi3FsJ$EnhMCR6iPrgkp3G1iulbz5dAw9apErh2UrbyVD6JQP7")
-db.session.add(rdkx_user)
-db.session.commit()
-
-infos1 = JoinInfos(id='1', name='机组负荷', groupA='421.88', groupB='421.88', unit='MW')
-infos2 = JoinInfos(id='2', name='烟气侧压差', groupA='1100', groupB='930', unit='Pa')
-infos3 = JoinInfos(id='3', name='堵塞系数', groupA='1.248', groupB='1.055', unit='')
-infos4 = JoinInfos(id='4', name='堵塞速度', groupA='-11.04', groupB='-4.12', unit='%/天')
-infos5 = JoinInfos(id='5', name='脱硝实际喷氨量', groupA='103.26', groupB='105.37', unit='kg/h')
-infos6 = JoinInfos(id='6', name='脱氨喷氨需求量', groupA='84.35', groupB='84.35', unit='kg/h')
-infos7 = JoinInfos(id='7', name='沉积系数', groupA='62334', groupB='60891', unit='')
-infos8 = JoinInfos(id='8', name='沉积系数12天均值', groupA='73776', groupB='70818', unit='')
-db.session.add_all([infos1, infos2, infos3, infos4, infos5, infos6, infos7, infos8])
-db.session.commit()
-
-# excel数据导入到数据库
-dataPath = '../src/data/excel'
-runtimeData = xlrd.open_workbook(dataPath+'/焦作数据.xlsx')
-sheet = runtimeData.sheet_by_name('historyData')
-table = runtimeData.sheet_by_name(u'historyData')
-list = []
-for row in range(sheet.nrows):
-    rowlist = []
-    for col in range(sheet.ncols):
-        value = sheet.cell(row,col).value        
-        if sheet.cell(row,col).ctype == 3:            
-            date = xldate_as_tuple(sheet.cell(row,col).value,0)            
-            value = datetime(*date)  # excel中读取时间格式数据要注意      
-            # print('value',value)
-        rowlist.append(value)
-    list.append(rowlist)
-
-del list[0] #删掉第一行，第一行获取的是文件的头，一般不用插到数据库里面
-# print('list', list)
-    # 将数据存入数据库
-for a in list:
-    historydatas = HistoryData()
-    historydatas.id = a[0]
-    # historydatas.time = a[1].to_dict()
-    historydatas.time = a[1]
-    # print('historydatas.time',historydatas.time)
-    historydatas.airClogA = a[2]
-    historydatas.airClogB = a[3]
-    historydatas.sizeNH3Actual = a[4]
-    historydatas.sizeNH3Demand = a[5]
-    historydatas.airDeposiA = a[6]
-    historydatas.airDeposiB = a[7]
-    # print('list a',historydatas)
-    db.session.add(historydatas)
-    db.session.commit()
-print(list[3][1])
-
-listAirClogA = []
-listAirClogB = []
-listSizeNH3Actual = []
-listSizeNH3Demand = []
-listAirDeposiA = []
-listAirDeposiB = []
-
-for row in range(sheet.nrows - 1):
-    listAirClogA.append({0:list[row][1], 1:list[row][2]})
-    listAirClogB.append({0:list[row][1], 1:list[row][3]})
-    listSizeNH3Actual.append({0:list[row][1], 1:list[row][4]})
-    listSizeNH3Demand.append({0:list[row][1], 1:list[row][5]})
-    listAirDeposiA.append({0:list[row][1], 1:list[row][6]})
-    listAirDeposiB.append({0:list[row][1], 1:list[row][7]})
 
 @app.route('/api/getdrawAirClogChart', methods=['GET'])
 @auth.login_required
@@ -333,7 +176,7 @@ def bathremove_user():
     else:
         return jsonify({'code': 500, 'msg': "未知错误"})
 
-# excel数据交互(还是没成功)
+# excel数据交互
 @app.route('/api/upload', methods=['GET', 'POST'])
 # @auth.login_required
 def upload():
